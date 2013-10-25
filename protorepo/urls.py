8d02b238a1452a7c2e7bbbf66d9cd637c974ec0e
Ryan Skonnord <rskonnord@plos.org>
2013-10-24 17:50:45 -0700

    Add first view to 'api' app

--- protorepo/urls.py
+++ protorepo/urls.py
@@ -4,10 +4,9 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'protorepo.views.home', name='home'),
-    # url(r'^protorepo/', include('protorepo.foo.urls')),
+urlpatterns = patterns(
+    '',
+    url(r'^api/', include('api.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
