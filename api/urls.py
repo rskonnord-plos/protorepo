19e5afdf12d74194fd70ea16fea635ba301ccabc
Ryan Skonnord <rskonnord@plos.org>
2013-10-24 18:13:33 -0700

    Add view that takes parameter

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 
 from api import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.index, name='index'),
+    url(r'^read/(?P<item_name>.*)', views.read, name='read'),
 )
--- api/views.py
+++ api/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
+
+def read(request, item_name):
+    return HttpResponse("Show value for: {0!r}.".format(item_name))
