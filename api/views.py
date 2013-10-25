7aa0d4b9af0298e3b627791098a1a6a448ba2f0a
Ryan Skonnord <rskonnord@plos.org>
2013-10-24 18:15:54 -0700

    Query database from view

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from api.models import Item
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
 
 def read(request, item_name):
-    return HttpResponse("Show value for: {0!r}.".format(item_name))
+    item = Item.objects.get(name=item_name)
+    return HttpResponse("Found item: {0!r}".format(item))
